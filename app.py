63f5cfd447b7b9be70c500d4e621424cc950bd53
AlphaBs <ksi123456ab@naver.com>
2025-08-11 18:52:40 +0900

    결과물 다운로드 URL

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 from config import get_config_all, load_config, save_config, set_config
-from flask import Flask, Response, jsonify, request
+from flask import Flask, Response, jsonify, request, send_file
 from flask_cors import CORS
 import os
 from werkzeug.exceptions import HTTPException
@@ -22,7 +22,7 @@ app = Flask(__name__)
 CORS(app)
 
 
-@app.route("/api/create", methods=["POST"])
+@app.route("/api/tasks", methods=["POST"])
 def create():
     try:
         if "options" not in request.form:
@@ -42,12 +42,22 @@ def create():
             file.save(file_path)
 
         video_task.run()
-        return jsonify({"message": "success", "status": "success"})
+        return jsonify({"status": "success", "id": video_task.task_id})
 
     except Exception as e:
         logging.exception("/api/create")
-        return jsonify({"message": "error", "status": "error", "error": str(e)}), 500
+        return jsonify({"status": "error", "error": str(e)}), 500
 
+
+@app.route("/api/tasks/<task_id>/result", methods=["GET"])
+def get_result(task_id):
+    try:
+        video_task = VideoTask.resume_from(task_id, {})
+        video_path = video_task.get_merged_video_path()
+        return send_file(video_path)        
+    except Exception as e:
+        logging.exception("/api/tasks/<task_id>/result")
+        return jsonify({"status": "error", "error": str(e)}), 500
 
 @app.route("/api/config", methods=["GET"])
 def get_config():
